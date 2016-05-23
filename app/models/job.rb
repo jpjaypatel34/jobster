7f2a91e21bcff967d0dee00a5eb0cf32bba20da5
Jay Patel <jpjaypatel34@gmail.com>
2016-05-23 13:21:02 +1200

    Second last commit

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,6 +2,6 @@ class Job < ActiveRecord::Base
 	belongs_to :category
 	belongs_to :user
 
-	has_attached_file :image, styles: { medium: "537x400#" }
+	has_attached_file :image, styles: { medium: "554x400#" }
   	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
